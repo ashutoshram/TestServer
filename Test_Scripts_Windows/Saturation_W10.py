9c96a4f7c53e3f4aeeae6d8e92a6171d19f85834
extktran <extktran@jabra.com>
2020-10-16 15:26:53 -0700

    modified saturation test

--- Test_Scripts_Windows/Saturation_W10.py
+++ Test_Scripts_Windows/Saturation_W10.py
@@ -4,12 +4,13 @@ import platform
 import numpy as np
 import sys
 import cv2
+from queue import Queue
 
-production = True
+production = False
 debug = False 
 if not production:
     import AbstractTestClass as ATC
-    import webcamPy as wpy
+    # import webcamPy as wpy
 else:
     import eos.scripts.AbstractTestClass as ATC
     import eos.scripts.webcamPy as wpy
@@ -30,8 +31,8 @@ class Saturation(ATC.AbstractTestClass):
         self.SaturationTest = SaturationTester()
         self.SaturationTest.test(args, q, results)
         print("Saturation.run: waiting for wait_q")
-        got = wait_q.get()
-        print("Saturation.run: got %s" % repr(got))
+        # got = wait_q.get()
+        # print("Saturation.run: got %s" % repr(got))
 
     def get_progress(self):
         if self.SaturationTest is None:
@@ -59,18 +60,22 @@ class Saturation(ATC.AbstractTestClass):
     def generate_report(self):
         return self.SaturationTest.results()
 
-
 class SaturationTester():
+    count = 0
 
     def __init__(self):
         self.err_code = {}
-        self.progress_percent = 0 
-        self.cam = wpy.Webcam()
-        print("hello")
-        if not self.cam.open(3840, 1080, 30.0, "YUY2"):
-            print("PanaCast cannot be opened!!")
-            #sys.exit(1)
-        print("opened panacast device")
+        self.progress_percent = 0
+        # set up camera stream
+        self.cam = cv2.VideoCapture(2)
+        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
+        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+        # check if camera stream exists
+        if self.cam is None:
+            print('cv2.VideoCapture unsuccessful')
+            sys.exit(1)
+        print(self.cam)
 
     def progress(self):
         return self.progress_percent
@@ -84,10 +89,10 @@ class SaturationTester():
             print(type(return_val))
             print(type(saturation_level))
             if return_val == int(saturation_level):
-                print("Hello")
+                print("Success")
                 self.err_code[saturation_level] = 0
             else:
-                print("goodbye")
+                print("Failure")
                 self.err_code[saturation_level] = -1
             self.progress_percent += 33
             q.put(self.progress_percent)
@@ -99,24 +104,36 @@ class SaturationTester():
 
     def test_saturation(self, saturation_level):
         print('entering test_saturation')
-        if not self.cam.setCameraControlProperty('saturation', saturation_level):
-            print('test_saturation: cannot set saturation')
-        time.sleep(3)
-        f = self.cam.getFrame()
-        bgr = cv2.cvtColor(f, cv2.COLOR_YUV2BGR_YUY2)
-        hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+        # set saturation and capture frame after three second delay
+        self.cam.set(cv2.CAP_PROP_SATURATION, saturation_level)
+        t_end = time.time() + 3
+        while True:
+            ret, frame = self.cam.read()
+            if time.time() > t_end:
+                img = "test_saturation" + "_{}".format(SaturationTester.count) + ".png"
+                cv2.imwrite(img, frame)
+                print("{} captured".format(img))
+                # print(frame)
+                break
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         h, s, v = cv2.split(hsv)
         saturation_average = np.average(s)
-        print(saturation_average)
-        current_saturation = self.cam.getCameraControlProperty('saturation')[0]
-        default_saturation = self.cam.getCameraControlProperty('saturation')[3]
-        self.cam.setCameraControlProperty('hdr', default_saturation)
-        #print(current_saturation)
+
+        print("Saturation average: {}".format(saturation_average))
+        current_saturation = self.cam.get(cv2.CAP_PROP_SATURATION)
+        print("Current saturation: {}".format(current_saturation))
+        SaturationTester.count += 1
         return current_saturation
 
 
 if __name__ == "__main__":
-	t = Saturation()
-	args = t.get_args()
-	t.run(args)
-	print(t.generate_report())
+    t = Saturation()
+    args = t.get_args()
+    q = Queue()
+    results = Queue()
+    wait_q = Queue()
+    t.run(args, q, results, wait_q)
+
+    print("Generating report...")
+    print(t.generate_report())
--- Test_Scripts_Windows/cvtest.py
+++ Test_Scripts_Windows/cvtest.py
@@ -9,38 +9,29 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 cv2.namedWindow("capture", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("capture", (1280, 720))
 
-fps = [30.0, 27.0, 24.0, 15.0, 30.0]
+saturation = [128, 136, 160, 176, 155]
 i = 0
 
 while True:
-    # retval, frame = cam.read()
-    # cv2.imshow("capture", frame)
+    retval, frame = cam.read()
+    cv2.imshow("capture", frame)
 
-    # if retval is not True:
-    #     break
-
-    # k = cv2.waitKey(1)
-    # # ESC pressed
-    # if k%256 == 27:
-    #     print("Escape hit, closing...")
-    #     break
-    # # SPACE pressed
-    # elif k%256 == 32:
-    #     if i >= len(fps):
-    #         break
-    #     print("Current FPS: {}".format(cam.get(cv2.CAP_PROP_FPS)))
-    #     cam.set(cv2.CAP_PROP_FPS, fps[i])
-    #     print("FPS set to {}".format(cam.get(cv2.CAP_PROP_FPS)))
-    #     i += 1
-
-    if i >= len(fps):
+    if retval is not True:
         break
-    print("Current FPS: {}".format(cam.get(cv2.CAP_PROP_FPS)))
-    print("Setting FPS to: {}".format(fps[i]))
-    cam.set(cv2.CAP_PROP_FPS, fps[i])
-    time.sleep(3)
-    print("FPS set to {}".format(cam.get(cv2.CAP_PROP_FPS)))
-    i += 1
+
+    k = cv2.waitKey(1)
+    # ESC pressed
+    if k%256 == 27:
+        print("Escape hit, closing...")
+        break
+    # SPACE pressed
+    elif k%256 == 32:
+        if i >= len(saturation):
+            break
+        print("Current saturation: {}".format(cam.get(cv2.CAP_PROP_SATURATION)))
+        cam.set(cv2.CAP_PROP_SATURATION, saturation[i])
+        print("saturation set to {}".format(cam.get(cv2.CAP_PROP_SATURATION)))
+        i += 1
 
 cam.release()
 cv2.destroyAllWindows()
