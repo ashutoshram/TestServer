f8069271cd8c93f37c57c97fd9728fa7d2cf3d21
altia <khtran@jabra.com>
2021-03-29 17:40:06 -0700

    added test report for ResolutionSwitch

--- Test_Scripts_Linux/ResolutionSwitch.py
+++ Test_Scripts_Linux/ResolutionSwitch.py
@@ -6,6 +6,7 @@ import subprocess
 import argparse
 import json
 import re
+import pprint as p
 from datetime import date, datetime
 
 ap = argparse.ArgumentParser()
@@ -31,6 +32,7 @@ current = date.today()
 path = os.getcwd()
 device_num = 0
 reboots = 0
+err_code = {}
 
 def log_print(args):
     msg = args + "\n"
@@ -54,7 +56,7 @@ def reboot_device():
     if power_cycle is True:
         subprocess.check_call(['./power_switch.sh', '0', '0'])
         time.sleep(3)
-        subprocess.check_call(['./power_switch.sh', '0', '0'])
+        subprocess.check_call(['./power_switch.sh', '0', '1'])
     else:
         os.system("sudo adb kill-server")
         os.system("sudo adb devices")
@@ -80,6 +82,8 @@ def reboot_device():
 
 def test_fps(width, height, target_res, frame_count):
     start_frame, test_frame, total_frame, drop_frame = (0 for x in range(4))
+    all_fps = []
+    global err_code
     # set initial resolution
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
@@ -90,6 +94,7 @@ def test_fps(width, height, target_res, frame_count):
         if target[0] == width and target[1] == height:
             continue
         else:
+            test_type = "{}x{} -> {}x{}".format(width, height, target[0], target[1])
             cap.set(cv2.CAP_PROP_FRAME_WIDTH, target[0])
             cap.set(cv2.CAP_PROP_FRAME_HEIGHT, target[1])
             # check when frame is available to grab after switching resolutions
@@ -103,7 +108,7 @@ def test_fps(width, height, target_res, frame_count):
                     break
 
         log_print(55*"=")
-        log_print("{}x{} -> {}x{}".format(width, height, target[0], target[1]))
+        log_print(test_type)
         log_print("Time to switch (ms):   {}\n".format(switch_time * 1000))
         test_start, test_time = (time.time() for x in range(2))
         # grab frames for 30 seconds (900 frames)
@@ -132,8 +137,21 @@ def test_fps(width, height, target_res, frame_count):
 
                 fps = float(test_frame / current)
                 log_print("Current average fps:   {:<5}\n".format(fps))
+                all_fps.append(fps)
                 test_time = time.time()
                 test_frame = 0
+        
+        # write pass/fail report to dict
+        avg_fps = sum(all_fps) / len(all_fps)
+        if switch_time < 1.4:
+            if avg_fps >= 29:
+                err_code[test_type] = 1
+            elif avg_fps < 29 and avg_fps >= 27:
+                err_code[test_type] = 0
+            else:
+                err_code[test_type] = -1
+        else:
+            err_code[test_type] = -1
 
         # reset to starting resolution
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
@@ -163,6 +181,12 @@ if __name__ == "__main__":
             width = start[0]
             height = start[1]
             test_fps(width, height, target_res, frame_count)
+    
+    log_print("\nGenerating report...")
+    log_print("Number of video crashes/freezes (that required reboots): {}".format(reboots))
+    report = p.pformat(err_code)
+    log_print("{}\n".format(report))
+    log_file.close()
 
     cap.release()
     cv2.destroyAllWindows()
