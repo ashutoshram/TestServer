4caf59eb77c19a0ac85832653c10817e56522722
altia <khtran@jabra.com>
2021-05-12 13:41:52 -0700

    fixed some log formatting

--- Test_Scripts_Linux/frame.py
+++ Test_Scripts_Linux/frame.py
@@ -12,7 +12,7 @@ def brightness(device, cap, ctrl, debug, log_file):
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     for c in ctrl:
-        logprint.send("\nBrightness:             {:<5}".format(c), debug, log_file)
+        logprint.send("\nBrightness:  {:<5}".format(c), debug, log_file)
         subprocess.call(['{} -c brightness={}'.format(device, str(c))], shell=True)
         t_end = time.time() + 3
         while True:
@@ -23,7 +23,7 @@ def brightness(device, cap, ctrl, debug, log_file):
         # convert to grayscale and calculate luma
         f = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         luma = np.average(f)
-        logprint.send("luma:                   {:<5}".format(luma), debug, log_file)
+        logprint.send("luma:        {:<5}".format(luma), debug, log_file)
         results.append(luma)
 
     return results
@@ -35,7 +35,7 @@ def contrast(device, cap, ctrl, debug, log_file):
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     for c in ctrl:
-        logprint.send("\nContrast:             {:<5}".format(c), debug, log_file)
+        logprint.send("\nContrast:        {:<5}".format(c), debug, log_file)
         subprocess.call(['{} -c contrast={}'.format(device, str(c))], shell=True)
         t_end = time.time() + 3
         while True:
@@ -47,7 +47,7 @@ def contrast(device, cap, ctrl, debug, log_file):
         f = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         ret, thresh = cv2.threshold(f, 0, 255, cv2.THRESH_OTSU)
         otsu = np.average(thresh)
-        logprint.send("Otsu threshold:       {}".format(otsu), debug, log_file)
+        logprint.send("Otsu threshold:  {}".format(otsu), debug, log_file)
         results.append(otsu)
     
     return results
@@ -59,7 +59,7 @@ def saturation(device, cap, ctrl, debug, log_file):
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     for c in ctrl:
-        logprint.send("\nsaturation:             {:<5}".format(c), debug, log_file)
+        logprint.send("\nsaturation:            {:<5}".format(c), debug, log_file)
         subprocess.call(['{} -c saturation={}'.format(device, str(c))], shell=True)
         t_end = time.time() + 3
         while True:
@@ -71,7 +71,7 @@ def saturation(device, cap, ctrl, debug, log_file):
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         h, s, v = cv2.split(hsv)
         sat_avg = np.average(s)
-        logprint.send("Saturation (HSV) avg:       {}".format(sat_avg), debug, log_file)
+        logprint.send("Saturation (HSV) avg:  {}".format(sat_avg), debug, log_file)
         results.append(sat_avg)
     
     return results
@@ -82,7 +82,7 @@ def sharpness(device, cap, ctrl, debug, log_file):
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
     for c in ctrl:
-        logprint.send("\nsharpness:             {:<5}".format(c), debug, log_file)
+        logprint.send("\nsharpness:           {:<5}".format(c), debug, log_file)
         subprocess.call(['{} -c sharpness={}'.format(device, str(c))], shell=True)
         t_end = time.time() + 3
         while True:
@@ -93,7 +93,7 @@ def sharpness(device, cap, ctrl, debug, log_file):
         # convert to grayscale and calculate lapacian variance
         f = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         variance = cv2.Laplacian(f, cv2.CV_64F).var()
-        logprint.send("Laplacian variance:       {}".format(variance), debug, log_file)
+        logprint.send("Laplacian variance:  {}".format(variance), debug, log_file)
         results.append(variance)
     
     return results
@@ -107,7 +107,7 @@ def white_balance(device, cap, ctrl, debug, log_file):
         logprint.send("\nwhite_balance_temperature:  {:<5}".format(c), debug, log_file)
         subprocess.call(['{} -c white_balance_temperature={}'.format(device, str(c))], shell=True)
         wb = cap.get(cv2.CAP_PROP_TEMPERATURE)
-        logprint.send("White balance:              {}".format(wb), debug, log_file)
+        logprint.send("White balance temperature:    {}".format(wb), debug, log_file)
         results.append(wb)
     
     return results
