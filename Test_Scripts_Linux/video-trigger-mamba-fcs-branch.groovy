2c0891e84dec2ba4bb0c06c095d1048f02d1d20c
Khang Tran <khtran@jabra.com>
2021-06-30 15:29:29 -0700

    additional jenkins config files

---
+++ Test_Scripts_Linux/video-trigger-mamba-fcs-branch.groovy
@@ -0,0 +1,44 @@
+
+pipeline {
+    agent none
+
+    stages {
+        stage('prepare') {
+            agent {
+                docker {
+                    label 'cameratestcupertino'
+                    image "jabraesw/mamba-workspace:0.8"
+                }
+            }
+
+            stages {
+                stage('MAMBA_FCS_BRANCH') {
+                    steps {
+                        dir('platform') {
+                            checkout([
+                                $class: 'GitSCM',
+                                branches: [[name: 'MAMBA_FCS_BRANCH']],
+                                userRemoteConfigs: [[url: 'git@github.com:altiasystems/MX.git', credentialsId:'jenkins']]
+                            ])
+                        }
+                        script {
+                            if(currentBuild.changeSets.size() > 0) {
+                                def changeLogSets = currentBuild.changeSets
+                                def nrChanges = changeLogSets.size() + 1
+                                currentBuild.description = nrChanges + " changes"
+                                build job: 'video-test', parameters: [
+                                    string(name: 'BRANCH', value: 'MAMBA_FCS_BRANCH'),
+                                    booleanParam(name: 'DO_PYTHON', value: false),
+                                    booleanParam(name: 'DO_MAMBA', value: true),
+                                    booleanParam(name: 'RUN_ALL_TESTS', value: true)
+                                ], wait: true
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+}
